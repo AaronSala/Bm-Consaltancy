634150f90abd383e879e09ee5dc74bc5d5a103b9
Aaron Sala <salaaron2@gmail.com>
2025-07-17 23:01:56 +0300

    updated the year in the footer

--- src/components/videography/VideographyFooter.tsx
+++ src/components/videography/VideographyFooter.tsx
@@ -61,7 +61,7 @@ export const VideographyFooter = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 BM consoltants. All rights reserved.</p>
+          <p>&copy; 2025 BM consoltants. All rights reserved.</p>
         </div>
       </div>
     </footer>
